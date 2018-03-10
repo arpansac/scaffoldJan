e08c082a6149b88395c44e125bb8624ea25b4e01
arpansac <arpansac@gmail.com>
2018-03-10 16:49:07 +0530

    js requests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,10 @@
 class CommentsController < ApplicationController
   def create
-  	comment = Comment.create(
+  	@new_comment = Comment.create(
   			post_id: params[:comment][:post_id],
   			user_id: current_user.id,
   			content: params[:comment][:content]
   		)
-  	
-  	redirect_to "/posts/#{params[:comment][:post_id]}"
+  	# redirect_to "/posts/#{params[:comment][:post_id]}"
   end
 end
